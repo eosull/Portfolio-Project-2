86ea925e14beb87d7460b6be7293bc86b2527be8
eosull <osullivaneoin442@gmail.com>
2022-12-28 19:59:44 +0000

    Link datalist submit button to js function running

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ function calculateGoalsPerGame(){
     let games = document.getElementsByClassName('goal-games');
     let goals = document.getElementsByClassName('goal-amount');
 
-    
+
     console.log(player[1].innerHTML);
     console.log(games[1].innerHTML);
     console.log(goals[1].innerHTML);
