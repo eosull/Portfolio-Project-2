960fbec83ad64d1316bed0198ae357d4ad033dc3
eosull <osullivaneoin442@gmail.com>
2022-12-28 19:38:28 +0000

    Begin writing JS function to target elements in goals scored table and calculate goals per game

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+function calculateGoalsPerGame(){
+    let player = document.getElementsByClassName('goal-player');
+    let games = document.getElementsByClassName('goal-games');
+    let goals = document.getElementsByClassName('goal-amount');
+
+    
+    console.log(player[1].innerHTML);
+    console.log(games[1].innerHTML);
+    console.log(goals[1].innerHTML);
+}
