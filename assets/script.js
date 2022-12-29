e9f148b411dec58d5846220c41116a130b77cf82
eosull <osullivaneoin442@gmail.com>
2022-12-29 11:43:09 +0000

    Add button and assist per game functionality to assist section

--- assets/script.js
+++ assets/script.js
@@ -10,5 +10,17 @@ function calculateGoalsPerGame(playerNum){
     let gpg = parseFloat(goal/game).toFixed(2);
 
     document.getElementById('gpg-statement').innerHTML = (`${player} Scored ${gpg} Goals Per Game`);
+}
 
+function calculateAssistsPerGame(playerNum){
+    let playerList = document.getElementsByClassName('assist-player');
+    let player = playerList[playerNum].innerHTML;
+    let gameList = document.getElementsByClassName('assist-games');
+    let game = gameList[playerNum].innerHTML;
+    let assistList = document.getElementsByClassName('assist-amount');
+    let assist = assistList[playerNum].innerHTML;
+
+    let apg = parseFloat(assist/game).toFixed(2);
+
+    document.getElementById('apg-statement').innerHTML = (`${player} Provided ${apg} Assists Per Game`);
 }
